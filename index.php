5da4d77d19d02e4353e0707c21d592d9bc6b8657
buenoelian364-stack <buenoelian364@gmail.com>
2026-02-09 06:59:29 -0600

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -255,4 +258,5 @@ hs.src = ('//s10.histats.com/js15_as.js');
 <!-- Histats.com  END  -->
 </body>
 </html>
+
 
